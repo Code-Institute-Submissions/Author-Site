1acfebb45f6e9a88bddb9797a9005c322547ecda
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-02 15:38:57 +0200

    Added verbose names to UserProfile model fields

--- profiles/models.py
+++ profiles/models.py
@@ -10,14 +10,30 @@ class UserProfile(models.Model):
     A User Profile model for storing the default delivery
     information for the user
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_county = models.CharField(max_length=80, null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country ', null=True, blank=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile'
+    )
+    default_phone_number = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name='Phone Number'
+    )
+    default_street_address1 = models.CharField(
+        max_length=80, null=True, blank=True, verbose_name='Street Address 1'
+    )
+    default_street_address2 = models.CharField(
+        max_length=80, null=True, blank=True, verbose_name='Street Address 2'
+    )
+    default_town_or_city = models.CharField(
+        max_length=40, null=True, blank=True, verbose_name='Town or City'
+    )
+    default_county = models.CharField(
+        max_length=80, null=True, blank=True, verbose_name='County or State'
+    )
+    default_postcode = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name='Post Code'
+    )
+    default_country = CountryField(
+        blank_label='Country ', null=True, blank=True, verbose_name='Country'
+    )
 
     def __str__(self):
         return self.user.username
