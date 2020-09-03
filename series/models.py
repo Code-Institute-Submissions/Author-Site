6c8b3a329dc64f1471134cd0b12d6a13e73dae52
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-03 11:11:00 +0200

    Create app & model for Series

---
+++ series/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Series(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Series'
+
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    page_url = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return self.name
