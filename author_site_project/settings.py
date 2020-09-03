9ba4ee0553d7d79eb4b8bf6b92688c404f711c5f
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-03 11:15:10 +0200

    Create product app & model

--- author_site_project/settings.py
+++ author_site_project/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'crispy_forms',
+    'products',
     'profiles',
     'series',
     'static_pages',
---
+++ products/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+from series.models import Series
+
+
+class Product(models.Model):
+    _TYPE_OF_PRODUCT = [
+        ('hard_back_book', 'Hard Back Book'),
+        ('paper_back_book', 'Paper Back Book'),
+        ('e_book', 'E-Book'),
+        ('audio_book', 'Audio Book'),
+        ('merchandise', 'Merchandise'),
+    ]
+
+    # ALL PRODUCTS
+    series = models.ForeignKey(Series, on_delete=models.SET_NULL, null=True, related_name='products')
+    in_stock = models.BooleanField(default=True)
+    image = models.ImageField()
+    name = models.CharField(max_length=254)
+    product_type = models.CharField(choices=_TYPE_OF_PRODUCT, max_length=20)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    shipping = models.DecimalField(max_digits=6, decimal_places=2)
+    recommended_audience_age = models.IntegerField()
+
+    # ALL BOOKS
+    author = models.CharField(max_length=254, null=True, blank=True)
+    publish_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    publisher = models.CharField(max_length=254, null=True, blank=True)
+    isbn = models.CharField(max_length=254, null=True, blank=True)
+
+    # PHYSICAL BOOKS & E-BOOKS
+    page_count = models.IntegerField(null=True, blank=True)
+
+    # AUDIO BOOKS
+    run_time = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
+    narrator = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
