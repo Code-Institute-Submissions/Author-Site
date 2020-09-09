c9a44416a6e91229b54f2472fae3a35884af8775
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-09 14:40:52 +0200

    Fan art template with nav bar links + art uploaded

--- fan_art/views.py
+++ fan_art/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from .models import FanArt
+
 
 def fan_art_gallery(request):
-    return render(request, 'fan_art/fan_art_gallery.html')
+    """ A view to return all peices of fan art """
+
+    art_list = FanArt.objects.all()
+
+    context = {
+        'art_list': art_list
+    }
+
+    return render(request, 'fan_art/fan_art_gallery.html', context)
