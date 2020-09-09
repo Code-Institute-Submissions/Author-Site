1bdf2fa792b948390a3a3daa3256765e03977608
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-09 14:23:50 +0200

    Model & admin view for the fan art app

--- fan_art/admin.py
+++ fan_art/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import FanArt
 
-# Register your models here.
+
+class FanArtAdmin(admin.ModelAdmin):
+
+    class Meta:
+        verbose_name_plural = 'Fan Art'
+
+    list_display = (
+        'user_profile',
+        'title',
+        'series',
+        'publish_date',
+        'is_approved',
+    )
+
+    list_display_links = (
+        'user_profile',
+        'title',
+        'series',
+        'publish_date',
+    )
+
+    search_fields = (
+        'is_approved',
+        'user_profile',
+        'series',
+    )
+
+    list_per_page = 25
+
+
+admin.site.register(FanArt, FanArtAdmin)
--- fan_art/models.py
+++ fan_art/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+from profiles.models import UserProfile
+from series.models import Series
+
+
+class FanArt(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=254)
+    artist_name = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    is_approved = models.BooleanField(default=False)
+    publish_date = models.DateField(null=True, blank=True, default=date.today)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True, related_name='fan_art')
+    series = models.ForeignKey(Series, on_delete=models.SET_NULL, null=True, related_name='fan_art')
+
+    def __str__(self):
+        return f'Title: {self.title}, by user {self.user_profile.user}'
