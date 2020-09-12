4c075e9d3586be59a51602f14a0c37adadd8ba48
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-12 11:46:16 +0200

    Series pages templates, urls & styling - Images for pages in project static img folder - urls + views created - magic & pg templates - index image divs renamed for clarity - navbar links to the series pages - page styling

--- static_pages/urls.py
+++ static_pages/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
+    path('magic_door', views.magic_door, name='magic_door'),
+    path('parma_ham', views.parma_ham, name='parma_ham'),
 ]
--- static_pages/views.py
+++ static_pages/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'static_pages/index.html')
 
+
 def about(request):
     return render(request, 'static_pages/about.html')
+
+
+def magic_door(request):
+    return render(request, 'static_pages/magic_door.html')
+
+
+def parma_ham(request):
+    return render(request, 'static_pages/parma_ham.html')
