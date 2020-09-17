2cd887f23241b84c51b33d431be33ab78fc84bef
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-17 19:52:54 +0200

    Create payment intent & show stripe element in HTML -  Create stripe payment intent - initialize stripe & mount card element - Adding styling to stripe element in HTML

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -5,5 +5,35 @@ $(() => {
     $('#shipping-address-div').toggle();
   })
 
+  // Read stripe details from HTML hidden fields
+  const stripePublicKey = $('#stripe_public_key').val()
+  const clientSecret = $('#client_secret').val()
+
+  // Creating a new stripe instance
+  const stripe = Stripe(stripePublicKey);
+
+  // Stripe's default styling
+  const style = {
+    base: {
+      color: '#000',
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: 'antialiased',
+      fontSize: '16px',
+      '::placeholder': {
+        color: '#aab7c4'
+      }
+    },
+    invalid: {
+      color: '#dc3545',
+      iconColor: '#dc3545'
+    }
+  };
+
+  // Creating a new card element on our stripe instance
+  const elements = stripe.elements();
+  const card = elements.create('card', {style: style});
+
+  // mounting our created card element
+  card.mount('#card-element');
 
 })
--- checkout/views.py
+++ checkout/views.py
@@ -1,23 +1,24 @@
 from django.conf import settings
 from django.contrib import messages
-from django.http import JsonResponse, HttpResponseForbidden
 from django.shortcuts import render, redirect, reverse
 
 from .forms import OrderForm
+from shopping_basket.context_processors import shopping_basket
 
-import os
 import stripe
 
 
-stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 def checkout(request):
     """ Returns the page where users fill out the order form """
 
+    # Get the current shopping basket
+    current_basket = shopping_basket(request)['shopping_basket']
+
     # Check the users has items in their shopping basket
-    shopping_basket = request.session.get('shopping_basket', {})
-    if not shopping_basket:
+    if not current_basket['products']:
         messages.info(
             request,
             'Hey, looks like your bag is empty! Why not check out the shop?'
@@ -25,10 +26,22 @@ def checkout(request):
         redirect_url = reverse('products')
         return redirect(redirect_url)
 
+    # Calculate total for stripe (with no decimal places)
+    stripe_total = round(current_basket['grand_total'] * 100)
+
+    # Stripe payment intent
+    payment_intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    # Create our order form
     order_form = OrderForm()
 
     context = {
         'order_form': order_form,
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+        'client_secret': payment_intent.client_secret,
     }
 
     return render(request, 'checkout/checkout.html', context)
