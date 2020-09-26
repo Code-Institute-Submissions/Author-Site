93f90cd7bbcb209c9038d5a2bc91b9b3e79a5f1c
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-26 15:00:08 +0200

    Populate checkout fields with logged in users saved details

--- checkout/views.py
+++ checkout/views.py
@@ -151,7 +151,21 @@ def checkout(request):
         return redirect(redirect_url)
 
     # Create our order form
-    order_form = OrderForm()
+    if request.user.is_authenticated:
+        profile = request.user.profile
+        order_form = OrderForm(initial={
+            'full_name': profile.user.get_full_name(),
+            'email': profile.user.email,
+            'phone_number': profile.default_phone_number,
+            'payment_street_address1': profile.default_street_address1,
+            'payment_street_address2': profile.default_street_address2,
+            'payment_town_or_city': profile.default_town_or_city,
+            'payment_county': profile.default_county,
+            'payment_postcode': profile.default_postcode,
+            'payment_country': profile.default_country,
+        })
+    else:
+        order_form = OrderForm()
 
     context = {
         'order_form': order_form,
