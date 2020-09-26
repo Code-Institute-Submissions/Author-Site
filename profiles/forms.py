ff5dfb082fcd23b76ed844da7a93ac7fe32c3468
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-26 19:36:37 +0200

    Added change username to user details page - removed email, handled in allauth email page

--- profiles/forms.py
+++ profiles/forms.py
@@ -16,10 +16,8 @@ class UserProfileForm(forms.ModelForm):
         """
         super().__init__(*args, **kwargs)
 
-        #TODO: decide which filed should be auto focussed
-
         # AUTOFOCUS
-        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        self.fields['default_street_address1'].widget.attrs['autofocus'] = True
 
         # CUSTOMIZING THE FORM FIELDS
         for fieldname, field in self.fields.items():
@@ -38,7 +36,7 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', )
 
     def __init__(self, *args, **kwargs):
         """
@@ -49,12 +47,12 @@ class UserForm(forms.ModelForm):
 
         # CUSTOMIZING THE FORM FIELDS
         for fieldname, field in self.fields.items():
-            if fieldname != 'default_country':
-                if field.required:
-                    placeholder = f'{field.label} *'
-                else:
-                    placeholder = field.label
+            if field.required:
+                placeholder = f'{field.label} *'
+            else:
+                placeholder = field.label
 
-                field.widget.attrs['placeholder'] = placeholder
+            field.widget.attrs['placeholder'] = placeholder
 
             field.label = False
+            field.help_text = False
