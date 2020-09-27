3b9b5cfe00612d3d6e79e4bb106f2e7d4658804e
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-27 19:34:30 +0200

    Testing create_or_update_user_profile

---
+++ profiles/test_model.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+
+class TestUserProfile(TestCase):
+
+    def test_create_or_update_user_profile(self):
+        user_data = {
+            'username': 'username',
+            'password': 'password',
+            'first_name': 'first_name',
+            'last_name': 'last_name',
+            'email': 'email',
+        }
+        user = User.objects.create_user(**user_data)
+        self.assertTrue(UserProfile.objects.get(user=user.id))
+
