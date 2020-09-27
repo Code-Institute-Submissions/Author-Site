20c27e5a9b6991d969b1208f2c08f12b5d57f756
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-27 21:07:12 +0200

    Tests for fan_art - test_fan_art_approval_flow - test_delete_fan_art_view

---
+++ fan_art/test_views.py
@@ -0,0 +1,86 @@
+from django.test import TestCase
+from datetime import date
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from .models import FanArt
+import os
+
+
+class TestFanArtViews(TestCase):
+    user_data = {
+        'username': 'username',
+        'password': 'password',
+        'first_name': 'first_name',
+        'last_name': 'last_name',
+        'email': 'email',
+    }
+
+    fan_art_data = {
+        'title': 'title',
+        'description': 'description',
+        'publish_date': date.today,
+    }
+
+
+    def test_fan_art_approval_flow(self):
+        user = User.objects.create_user(**self.user_data)
+        self.client.force_login(user)
+        with open('author_site_project/static/img/logo.png', 'rb') as file_pointer:
+            fan_art_data = dict(self.fan_art_data, image=file_pointer)
+            response = self.client.post(reverse('add_fan_art'), fan_art_data)
+
+        fan_art = FanArt.objects.get(user_profile=user.profile, title=fan_art_data['title'])
+
+        # Testing newly added fan art defaults to not approved
+        self.assertFalse(fan_art.is_approved)
+
+        # Get all fan art, validate we get a page and that the
+        # new unapproved art is NOT on it
+        response = self.client.get(reverse('all_fan_art'))
+        self.assertEqual(response.status_code, 200)
+        page = response.context['fan_art']
+        self.assertEqual(page.paginator.num_pages, 1)
+        self.assertFalse(fan_art in page.object_list)
+
+        # Get the users own fan art, validate we get a page and that
+        # the user's unapproved art IS on it
+        response = self.client.get(reverse('user_fan_art'))
+        self.assertEqual(response.status_code, 200)
+        page = response.context['user_fan_art']
+        self.assertEqual(page.paginator.num_pages, 1)
+        self.assertTrue(fan_art in page.object_list)
+
+        fan_art.is_approved = True
+        fan_art.save()
+
+        # Get all fan art, validate we get a page and that the
+        # new art IS now on it and is approved
+        response = self.client.get(reverse('all_fan_art'))
+        self.assertEqual(response.status_code, 200)
+        page = response.context['fan_art']
+        self.assertEqual(page.paginator.num_pages, 1)
+        self.assertTrue(fan_art in page.object_list)
+
+        # Editing the fan art, and verifying that it has returned to unapproved
+        updated_fan_art_data = dict(self.fan_art_data, title='title2')
+        self.client.post(reverse('edit_fan_art', args=[fan_art.id]), updated_fan_art_data)
+        fan_art.refresh_from_db()
+        self.assertFalse(fan_art.is_approved)
+        self.assertEqual(fan_art.title, updated_fan_art_data['title'])
+
+
+    def test_delete_fan_art_view(self):
+        user = User.objects.create_user(**self.user_data)
+        self.client.force_login(user)
+        with open('author_site_project/static/img/logo.png', 'rb') as file_pointer:
+            fan_art_data = dict(self.fan_art_data, image=file_pointer)
+            response = self.client.post(reverse('add_fan_art'), fan_art_data)
+
+        fan_art = FanArt.objects.get(user_profile=user.profile, title=fan_art_data['title'])
+
+        # Testing that the delete fan art function deletes the art correctly
+        self.client.post(reverse('delete_fan_art', args=[fan_art.id]))
+        with self.assertRaises(FanArt.DoesNotExist):
+            fan_art = FanArt.objects.get(user_profile=user.profile, title=fan_art_data['title'])
+
