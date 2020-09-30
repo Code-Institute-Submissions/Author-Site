ebdd2360c632b44d666c39406b16c989aef49db9
LittleBlue418 <hollyathomas88@gmail.com>
2020-09-30 21:23:29 +0200

    Move SMTP settings to .env file

--- author_site_project/settings.py
+++ author_site_project/settings.py
@@ -204,14 +204,14 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET', '')
 
 # E-mails
-DEFAULT_FROM_EMAIL = 'avasmagicdoor@gmail.com'
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 if "EMAIL_HOST_PASSWORD" in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = '465'
-    EMAIL_HOST_USER = 'avasmagicdoor@gmail.com'
+    EMAIL_HOST = os.environ.get('EMAIL_HOST')
+    EMAIL_PORT = os.environ.get('EMAIL_PORT', '465')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', DEFAULT_FROM_EMAIL)
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-    EMAIL_USE_SSL = True
+    EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL','true').lower() == 'true'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
